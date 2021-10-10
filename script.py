19bbb3299c2014734fc230584b039be26d35a03c
pierrehoudayer <91544829+pierrehoudayer@users.noreply.github.com>
2021-10-10 15:16:33 +0200

    Update script.py
    Added MT98 function

--- script.py
+++ script.py
@@ -56,9 +56,15 @@ def plot_diagnostic(f, dk_f, dk_f_cov, show=False) :
 #============================================================================#
 #                                DIAGNOSTIC FIT                              #
 #============================================================================#
+def MT98(f, a, b, τ, φ):
+    '''Expression of the frequency shift caused by the 2nd He ionisation
+    derived in Monteiro & Thompson (1998) and used in Verma et al. (2014a)'''
+    return a * np.sin(2*np.pi*f*b)**2/(2*np.pi*f*b) * np.cos(2*np.pi*f*τ+φ)
+
+
 def HG07(f, a, b, τ, φ):
     '''Expression of the frequency shift caused by the 2nd He ionisation
-    visible in Houdek & Gough (2007)
+    derived in Houdek & Gough (2007)
     This expression were also applied to the second differences fit in 
     Verma et al. (2014a, 2019) and adapted in Farnir et al. (2019)'''
     return (a*f) * np.exp(-b*f**2) * np.cos(2*np.pi*f*τ+φ)
